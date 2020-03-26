f28b93968c5d8b06ce4fe8764a491321abd32ce4
Rofy-Ray <okyereforsonr@berea.edu>
2020-03-25 22:53:14 -0400

    ui revamped

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from flask import render_template
 app = Flask(__name__)
 
 
-
-
 with open("data/gofundme.json") as f:
     links = json.load(f)
 
@@ -15,7 +13,6 @@ with open("data/smallbusiness.json") as f:
 
 with open("data/resources.json") as f:
     resources = json.load(f)
-
 
 
 @app.route('/')
@@ -29,11 +26,11 @@ def home():
 @app.route('/relief')
 def business():
 
-    return render_template('relief.html',businesses= businesses)
+    return render_template('relief.html', businesses= businesses)
 
 @app.route('/contribute')
 def goFUndMe():
-    return render_template('contribute.html',contributions = links)
+    return render_template('contribute.html', contributions = links)
 
 @app.route('/resources')
 def help():
