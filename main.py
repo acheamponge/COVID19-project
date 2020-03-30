e6568183be22faefb5710fc95ea9ac4d9ff31eb6
Rofy-Ray <okyereforsonr@berea.edu>
2020-03-30 03:01:15 -0400

    dynamic item addition

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import json
 from flask import render_template
 app = Flask(__name__)
 
+with open("data/smallbusiness.json") as a:
+    businesses = json.load(a)
 
-with open("data/gofundme.json") as f:
-    links = json.load(f)
+with open("data/gofundme.json") as b:
+    contributions = json.load(b)
 
-with open("data/smallbusiness.json") as f:
-    businesses = json.load(f)
-
-with open("data/resources.json") as f:
-    resources = json.load(f)
+with open("data/resources.json") as c:
+    resources = json.load(c)
 
 
 @app.route('/')
@@ -25,12 +24,11 @@ def home():
 
 @app.route('/relief')
 def business():
-
-    return render_template('relief.html', businesses= businesses)
+    return render_template('relief.html', businesses = businesses)
 
 @app.route('/contribute')
 def goFUndMe():
-    return render_template('contribute.html', contributions = links)
+    return render_template('contribute.html', contributions = contributions)
 
 @app.route('/resources')
 def help():
